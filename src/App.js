8fdebc6333c64749b58dba51031c6597531337aa
vishalicious213 <vishalicious213@gmail.com>
2019-10-24 23:35:23 -0400

    numbers rendered. display rendered with initial state.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
+import Display from "../src/components/DisplayComponents/Display";
+import Numbers from "../src/components/ButtonComponents/NumberButtons/Numbers";
+
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
 
@@ -13,11 +16,15 @@ function App() {
   // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
   // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+  const [display, setDisplay] = useState(0);
+
   return (
     <div className="container">
       <Logo />
       <div className="App">
         {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
+        <Display display={display}/>
+        <Numbers />
       </div>
     </div>
   );
--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, {useState} from "react";
 
 //import any components needed
 // example of import from data.js. Note all the ../   This is how we move through folders. 
 
-import { numbers } from '../../../data' 
+import { numbers } from '../../../data';
+import NumberButton from './NumberButton';
 
 //Import your array data to from the provided data file
 
@@ -14,7 +15,15 @@ const Numbers = () => {
     <div>
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+       it any props needed by the child component*/
+       numberState.map((num, index) => {
+         return <NumberButton num={num} key={index}/>         
+       }
+
+       )
+       }
     </div>
   );
 };
+
+export default Numbers;
